b8ffbf219372f57bda069e454166358831cc0a32
Mohammad Sulaman Usman <msushami@gmail.com>
2017-07-09 16:53:25 -0400

    ASP.net
    Creating Just a HomePage. Home Page have a BootStrap Slider Using JQuery

---
+++ classroom/classroom/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace classroom
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
